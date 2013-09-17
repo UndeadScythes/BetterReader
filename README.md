0cc64f0a9457003c082e73752159d20cd0718248
UndeadScythes <daveyognaut2@gmail.com>
2013-09-17 10:53:46 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# BetterReader - 1.0.2 #
+
+Born from my own obsessive compulsive need for tidy code comes
+BetterReader. A wrapper class for BufferedReaders that offers a nice way to
+access data within loops. No more headaches over the following code:
+```java
+while ((String line = reader.readLine()) != null) {
+    // Do something
+}
+
+while (true) {
+    String line = reader.readLine();
+    if (line == null) break;
+    // Do something
+}
+
+for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+    // Do something
+}
+
+String line = reader.readLine();
+while (line != null) {
+    // Do something
+    line = reader.readLine();
+}
+```
+Now we can use BetterReader to tidy up our code:
+```java
+while (reader.hasNext()) {
+    line = reader.getLine();
+    // Do something
+}
+```
+
+&copy; 2013 UndeadScythes of UndeadScythes' Empire
+
+Feel free to post issues and suggestions on [GitHub](https://github.com/UndeadScythes/BetterReader).
+BetterReader source code is released under the CC BY-NC-SA 2.0 UK license.
+A copy of this licence is located in the file named LICENCE.lic, if this file is
+not present the text can be found [here](http://creativecommons.org/licenses/by-nc-sa/2.0/uk/legalcode).
+
+Last edited on 2013-09-17.
---
+++ src/main/java/com/undeadscythes/betterreader/BetterReader.java
@@ -0,0 +1,82 @@
+package com.undeadscythes.betterreader;
+
+import java.io.*;
+import java.util.logging.*;
+import static java.util.logging.Logger.*;
+
+/**
+ * Just one little class to make the business of reading from files a little
+ * easier.
+ *
+ * @author UndeadScythes
+ */
+public class BetterReader extends BufferedReader {
+    private static final Logger LOGGER = getLogger(BetterReader.class.getName());
+
+    private String line;
+    private int lineNo;
+
+    /**
+     * Wrap a standard {@link Reader}.
+     *
+     * @param reader
+     */
+    public BetterReader(final Reader reader) {
+        super(reader);
+        lineNo = 0;
+    }
+
+    /**
+     * Wrap an {@link InputStream}.
+     *
+     * @param input
+     */
+    public BetterReader(final InputStream input) {
+        this(new InputStreamReader(input));
+    }
+
+    /**
+     * Wrap a {@link File}.
+     *
+     * @param file
+     * @throws FileNotFoundException
+     */
+    public BetterReader(final File file) throws FileNotFoundException {
+        this(new FileReader(file));
+    }
+
+    /**
+     * This method increments the line counter and reads the next line of the
+     * input source. The line read can be retrieved using
+     * {@link BetterReader#getLine}, calling this method twice will ignore the
+     * first line read.
+     *
+     * @return True if there was another line to be read
+     */
+    public boolean hasNext() {
+        try {
+            line = super.readLine();
+            lineNo++;
+        } catch (IOException ex) {
+            LOGGER.severe(ex.getMessage());
+            return false;
+        }
+        return line != null;
+    }
+
+    /**
+     *
+     * @return Returns the last line read
+     */
+    public String getLine() {
+        return line;
+    }
+
+    /**
+     *
+     * @return Current line number
+     */
+    public int getLineNo() {
+        return lineNo;
+    }
+}
